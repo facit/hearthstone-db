6b0249e13efba8bd0ee59fda9d6c82d677454458
Pontus Welin <pontus.welin@gmail.com>
2016-02-07 00:00:14 +0100

    Input of database file path as options.

--- kivy.py
+++ kivy.py
@@ -1,15 +1,34 @@
+import os
+import sys
 import sqlite3
 import kivy
-import optparse
+from optparse import OptionParser
 
 def parseOptions():
+    parser = OptionParser()
     parser.add_option("-d", "--database", dest="database",
                       help="write report to FILE", metavar="FILE")
+    (options, args) = parser.parse_args()
+    if options.database is None:
+        databases = []
+        for item in os.listdir():
+            if item.endswith(".db"):
+                databases.append(item)
+        if len(databases) == 1:
+            options.database = databases[0]
+        else:
+            sys.exit("What database file should be used? Use -d flag to define this.")
+    if not os.path.isfile(options.database):
+        sys.exit("No such file \"" + options.database + "\"")
 
-def main():
-    conn = sqlite3.connect('C:\\Users\\Pontus\\Documents\\workspace\\HSDeckBuilder\\hearthstone-db\\all-cards.db')
+    return (options, args)
+
+def main(options):
+    conn = sqlite3.connect(options.database)
     c = conn.cursor()
+    c.execute("SELECT * FROM cards")
+    print(c.fetchone())
 
 if __name__ == "__main__":
-    (args, optione) = parseOptions()
-    main()
+    (options, args) = parseOptions()
+    main(options)
